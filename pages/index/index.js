fac0705981e691de0da624a383ca981fe7067681
xiechong <178141346@qq.com>
2025-03-16 21:25:42 +0800

    devlop 首页页面初步实现

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+Page({
+  data: {
+      communityNotice: '这是最近的社区公告内大苏打实打实大苏打填实打实打算大撒大撒',
+      hotItems: [
+          {
+              id: 1,
+              imageUrl: "/image/swiper/1.png",
+              title: ""
+          },
+          {
+              id: 2,
+              imageUrl: "/image/swiper/2.png",
+              title: ""
+          },
+          {
+              id: 3,
+              imageUrl: "/image/swiper/3.png",
+              title: "热点 3"
+          }
+      ]
+  }
+});    
