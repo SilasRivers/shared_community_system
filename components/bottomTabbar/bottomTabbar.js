296683061ad24722081b08a315de6bc3b808ee97
xiechong <178141346@qq.com>
2025-04-01 20:37:05 +0800

    refactor 底部导航栏修改

--- components/bottomTabbar/bottomTabbar.js
+++ components/bottomTabbar/bottomTabbar.js
@@ -8,31 +8,34 @@ Component({
   data: {
     activeIndex: 0 // 用于记录当前被点击的导航项索引，初始值为 -1 表示没有被点击
   },
+  
   methods: {
+    switchToIndex(){
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    },
+    switchToOrder(){
+      wx.switchTab({
+        url: '/pages/order/order',
+      })
+    },
+    switchToMe(){
+      wx.switchTab({
+        url: '/pages/me/me',
+      })
+    },
+    switchToItem(){
+      wx.switchTab({
+        url: '/pages/items/items',
+      })
+    },
     navigateToPage(e) {
       console.log("点击")
-      // const index = e.currentTarget.dataset.index;
       const url = e.currentTarget.dataset.url;
-      // const newNavItems = this.properties.navItems.map((item, i) => {
-      //   return {
-      //     ...item,
-      //     isActive: i === index
-      //   };
-      // });
-      // this.setData({
-      //   activeIndex: index,
-      //   navItems: newNavItems
-      // });
-      // if (url.startsWith('/pages')) {
-      //   wx.switchTab({
-      //     url: url
-      //   });
-      // } else {
         wx.navigateTo({
           url: url
         });
       }
-      // this.triggerEvent('navigate', { url }); // 触发事件通知父组件进行页面跳转
-    // }
   }
 });    
