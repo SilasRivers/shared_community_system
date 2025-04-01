985f701cb63fb5d2f1ea43b999093f9d90a9e29c
xiechong <178141346@qq.com>
2025-04-01 20:35:31 +0800

    devlop 主页查询时分页加载商品提高性能

---
+++ cloudfunctions/searchGoodsListByPage/index.js
@@ -0,0 +1,29 @@
+const cloud = require('wx-server-sdk')
+const env = "cloud1-2gtx0pq9d4989b4f"
+cloud.init({
+  env: env
+})
+const db = cloud.database()
+exports.main = async (event, context) => {
+  const { page = 1, pageSize = 10 } = event;
+  const skipCount = (page - 1) * pageSize;
+
+  try {
+    const goodsList = await db.collection('demands')
+      .where({ 'status': false })
+      .orderBy('create_time', 'desc')
+      .skip(skipCount)
+      .limit(pageSize)
+      .get();
+    // 获取总数据量
+    const totalRes = await db.collection('demands').count();
+    const total = totalRes.total;
+    return {
+      list: goodsList.data,
+      total
+    };
+  } catch (error) {
+    console.error('云函数查询失败:', error);
+    throw new Error('服务器错误');
+  }
+};
--- pages/index/index.js
+++ pages/index/index.js
@@ -18,7 +18,13 @@ Page({
         isActive: true
       }
     ],
-    selectedTabIndex: 0 // 初始选中的索引
+    selectedTabIndex: 0, // 初始选中的索引,
+    // 商品加载
+    goodsList: [],       // 商品列表数据
+    currentPage: 1,             // 当前页码
+    pageSize: 10,        // 每页显示数量
+    hasMore: true,        // 是否还有更多数据,
+    isLoading: false
   },
 
   onShow: function () {
@@ -42,22 +48,40 @@ Page({
       }, fail: err => {
         console.error('调用云函数失败', err)
       }
-    })
+    }),
+      this.loadGoodsList();
+  },
+  async loadGoodsList() {
+    const { currentPage, pageSize, hasMore, isLoading } = this.data;
+    if (!hasMore||isLoading) {
+      console.log("没有了")
+      return;
+    }
+    try {
+      console.log("进来了")
+      this.setData({ isLoading: true });
+      const res = await wx.cloud.callFunction({
+        name: 'searchGoodsListByPage',
+        data: {
+          page: currentPage,
+          pageSize: pageSize
+        }
+      });
+      const { list, total } = res.result;
+      const newHasMore = currentPage * pageSize < total;
+      this.setData({
+        goodsList: this.data.goodsList.concat(list),
+       currentPage:currentPage+1,
+        hasMore: newHasMore,
+        isLoading: false
+      });
+    } catch (error) {
+      console.error('加载商品列表失败:', error);
+    }
+  },
+  // 滚动到底部触发加载
+  onReachBottom() {
+    this.loadGoodsList();
   }
-  //   handleNavigate(e) {
-  //     const { url } = e.detail;
-  //     console.log("点击啦")
-  //     // 这里可以根据需要进行额外的处理，比如页面跳转前的一些逻辑判断等
-  //     if (url.startsWith('/pages')) {
-  //       wx.switchTab({
-  //         url: url
-  //       });
-  //     } else {
-  //       wx.navigateTo({
-  //         url: url
-  //       });
-  //     }
-  //   }
-  // }
 
 });    
